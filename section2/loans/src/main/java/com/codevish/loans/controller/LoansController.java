ce43830a968ce1a642fca75013b9fd2a8e85af4c
Vishnu Chaturvedi <codevish0101@gmail.com>
2024-07-07 14:54:17 +0530

    Updated Swagger UI documentation for loans microservice

--- section2/loans/src/main/java/com/codevish/loans/controller/LoansController.java
+++ section2/loans/src/main/java/com/codevish/loans/controller/LoansController.java
@@ -1,9 +1,16 @@
 package com.codevish.loans.controller;
 
 import com.codevish.loans.constants.LoansConstants;
+import com.codevish.loans.dto.ErrorResponseDto;
 import com.codevish.loans.dto.LoansDto;
 import com.codevish.loans.dto.ResponseDto;
 import com.codevish.loans.service.ILoansService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -13,6 +20,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Loans in Codevish",
+        description = "CRUD REST APIs in Codevish to CREATE, UPDATE, FETCH AND DELETE loan details"
+)
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
@@ -21,6 +32,20 @@ public class LoansController {
 
     private ILoansService iLoansService;
 
+    @Operation(
+            summary = "Create Loan REST API",
+            description = "REST API to create new loan inside Codevish",
+            responses = {
+                @ApiResponse(
+                        responseCode = "201",
+                        description = "HTTP Status CREATED"
+                ),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "HTTP status Internal Server Error"
+                )
+            }
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createLoan(@Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits") @RequestParam String mobileNumber) {
         iLoansService.createLoan(mobileNumber);
@@ -28,6 +53,20 @@ public class LoansController {
                 .body(new ResponseDto(LoansConstants.STATUS_201, LoansConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Fetch Loan Details REST API",
+            description = "REST API to fetch loan details based on a mobile number",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "HTTP Status OK"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "HTTP Status Internal Server Error"
+                    )
+            }
+    )
     @GetMapping("/fetch")
     public ResponseEntity<LoansDto> fetchLoanDetails(@Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits") @RequestParam String mobileNumber) {
         LoansDto loansDto = iLoansService.fetchLoan(mobileNumber);
@@ -35,6 +74,29 @@ public class LoansController {
                 body(loansDto);
     }
 
+    @Operation(
+            summary = "Update Loan Details REST API",
+            description = "REST API to update loan details based on a loan number",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "HTTP Status OK"
+                    ),
+                    @ApiResponse(
+                            responseCode = "417",
+                            description = "Expectation Failed"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "HTTP Status Internal Server Error",
+                            content = @Content(
+                                    schema = @Schema(
+                                        implementation = ErrorResponseDto.class
+                                    )
+                            )
+                    )
+            }
+    )
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateLoanDetails(@Valid @RequestBody LoansDto loansDto) {
         boolean isUpdated = iLoansService.updateLoan(loansDto);
@@ -47,6 +109,29 @@ public class LoansController {
         }
     }
 
+    @Operation(
+            summary = "Delete Loan Details REST API",
+            description = "REST API to delete Loan details based on a mobile number",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "HTTP Status OK"
+                    ),
+                    @ApiResponse(
+                            responseCode = "417",
+                            description = "Expectation Failed"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "HTTP Status Internal Server Error",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = ErrorResponseDto.class
+                                    )
+                            )
+                    )
+            }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteLoanDetails(@Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits") @RequestParam String mobileNumber) {
         boolean isDeleted = iLoansService.deleteLoan(mobileNumber);
